7c9f77aa879b92fba040ec8418a7e2ca923e05b2
Kenny Dubroff <kennydubroff@hotmail.com>
2023-06-11 10:19:15 -0700

    loaded env

--- welcome_bot.py
+++ welcome_bot.py
@@ -1,9 +1,13 @@
+from dotenv import load_dotenv
+import logging
 import os
 import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
-intents.members = True  # Subscribe to the privileged members intent.
+intents.members = True
+
+logging.basicConfig(level=logging.INFO)
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
@@ -18,7 +22,10 @@ async def on_member_join(member):
         roadmap_channel = discord.utils.get(member.guild.channels, name="roadmap")
         general_information_channel = discord.utils.get(member.guild.channels, name="general_information")
         
-        await channel.send(f'Welcome {member.mention}! Glad to have you with us!\n\nGeneral Information: {general_information_channel.mention}\nRoadmap: {roadmap_channel.mention}')
-
+        await channel.send(f"""Welcome {member.mention}! Ready to pursue your dreams?
+                           
+                           General Information: {general_information_channel.mention}
+                           Roadmap: {roadmap_channel.mention}""")
+load_dotenv()
 DISCORD_TOKEN=os.getenv("DISCORD_TOKEN")
 bot.run(DISCORD_TOKEN)
